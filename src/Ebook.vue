ff85d0a309d4fa950c0fa9ae5896d057f1e25d47
zhongzhaoguo <248686477@qq.com>
2019-12-10 17:11:27 +0800

    字号选择 主题切换

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -12,7 +12,14 @@
              @click="nextPage"></div>
       </div>
     </div>
-    <menu-bar :ifTitleAndMenuShow="ifTitleAndMenuShow"></menu-bar>
+    <menu-bar :ifTitleAndMenuShow="ifTitleAndMenuShow"
+              :fontSizeList="fontSizeList"
+              :defaultFontSize="defaultFontSize"
+              @setFonSize="setFonSize"
+              :themeList="themeList"
+              :defaultTheme="defaultTheme"
+              @setTheme="setTheme"
+              ref="menuBar"></menu-bar>
   </div>
 </template>
 
@@ -28,10 +35,70 @@ export default {
   },
   data () {
     return {
-      ifTitleAndMenuShow: false
+      ifTitleAndMenuShow: false,
+      fontSizeList: [
+        { fontSize: 12 },
+        { fontSize: 14 },
+        { fontSize: 16 },
+        { fontSize: 18 },
+        { fontSize: 20 },
+        { fontSize: 22 },
+        { fontSize: 24 }
+      ],
+      defaultFontSize: 16,
+      defaultTheme: 0,
+      themeList: [
+        {
+          name: 'default',
+          style: {
+            body: {
+              'color': '#000', 'background': '#fff'
+            }
+          }
+        },
+        {
+          name: 'eye',
+          style: {
+            body: {
+              'color': '#000', 'background': '#ceeaba'
+            }
+          }
+        },
+        {
+          name: 'night',
+          style: {
+            body: {
+              'color': '#fff', 'background': '#000'
+            }
+          }
+        },
+        {
+          name: 'gold',
+          style: {
+            body: {
+              'color': '#000', 'background': 'rgb(241,236,226)'
+            }
+          }
+        }
+      ]
     }
   },
   methods: {
+    setTheme (index) {
+      this.theme.select(this.themeList[index].name)
+      this.defaultTheme = index
+    },
+    registerTheme () {
+      this.themeList.forEach(theme => {
+        this.theme.register(theme.name, theme.style)
+      })
+    },
+    setFonSize (fontSize) {
+      if (this.theme) {
+        this.theme.fontSize(fontSize + 'px')
+        this.defaultFontSize = fontSize
+      }
+    },
     prevPage () {
       if (this.rendition) {
         this.rendition.prev()
@@ -39,6 +106,9 @@ export default {
     },
     toggleTitleAndMenu () {
       this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
+      if (!this.ifTitleAndMenuShow) {
+        this.$refs.menuBar.hideSetting()
+      }
     },
     nextPage () {
       if (this.rendition) {
@@ -55,6 +125,10 @@ export default {
       })
       // 通过Rendition.display渲染电子书
       this.rendition.display()
+      this.theme = this.rendition.themes
+      this.theme.fontSize(this.defaultFontSize + 'px')
+      this.registerTheme()
+      this.setTheme(this.defaultThem)
     }
   },
   mounted () {
--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -2,18 +2,60 @@
   <div class="menu-bar">
     <transition name="slide-up">
       <div class="menu-wrapper"
+           :class="{'hide-box-shadow':ifSettingShow||!ifTitleAndMenuShow}"
            v-show="ifTitleAndMenuShow">
         <div class="icon-wrapper">
           <span class="icon-menu icon"></span>
           <span class="icon-progress icon"></span>
-          <span class="icon-bright icon"></span>
-          <span class="icon-a icon">A</span>
+          <span class="icon-bright icon"
+                @click="showSetting(1)"></span>
+          <span class="icon-a icon"
+                @click="showSetting(0)">A</span>
         </div>
       </div>
     </transition>
-    <div class="setting-wrapper">
-      <div class="setting-font-size"></div>
-    </div>
+    <transition name="slide-up">
+      <div class="setting-wrapper"
+           v-show="ifSettingShow">
+        <div class="setting-font-size"
+             v-if="showTag===0">
+          <div class="preview"
+               :style="{fontSize:fontSizeList[0].fontSize+'px'}">A</div>
+          <div class="select">
+            <div class="select-wrapper"
+                 v-for="(item,index) in fontSizeList"
+                 :key="index"
+                 @click="setFontSize(item.fontSize)">
+              <div class="line"></div>
+              <div class="point-wrapper">
+                <div class="point"
+                     v-show="defaultFontSize===item.fontSize">
+                  <div class="small-point">
+
+                  </div>
+                </div>
+              </div>
+              <div class="line"></div>
+            </div>
+          </div>
+          <div class="preview"
+               :style="{fontSize:fontSizeList[fontSizeList.length-1].fontSize+'px'}">A</div>
+        </div>
+        <div class="setting-theme"
+             v-else-if="showTag===1">
+          <div class="setting-theme-item"
+               v-for="(item,index) in themeList"
+               :key="index"
+               @click="setTheme(index)">
+            <div class="preview"
+                 :style="{background:item.style.body.background}"
+                 :class="{'no-border':item.style.body.background!=='#fff'}"></div>
+            <div class="text"
+                 :class="{'selected':index===defaultTheme}">{{item.name}}</div>
+          </div>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -22,6 +64,31 @@ export default {
   props: {
     ifTitleAndMenuShow: {
       type: Boolean, default: false
+    },
+    fontSizeList: Array,
+    defaultFontSize: Number,
+    themeList: Array,
+    defaultTheme: Number
+  },
+  data () {
+    return {
+      ifSettingShow: false,
+      showTag: 0
+    }
+  },
+  methods: {
+    setTheme (index) {
+      this.$emit('setTheme', index)
+    },
+    setFontSize (fontSize) {
+      this.$emit('setFonSize', fontSize)
+    },
+    showSetting (tag) {
+      this.ifSettingShow = true
+      this.showTag = tag
+    },
+    hideSetting () {
+      this.ifSettingShow = false
     }
   }
 }
@@ -29,7 +96,7 @@ export default {
 
 <style lang='scss' scoped>
 @import "../assets/styles/global";
-.meanu-bar {
+.menu-bar {
   .menu-wrapper {
     position: absolute;
     bottom: 0;
@@ -40,6 +107,9 @@ export default {
     height: px2rem(48);
     background: white;
     box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, 0.15);
+    &.hide-box-shadow {
+      box-shadow: none;
+    }
     .icon-wrapper {
       flex: 1;
       @include center;
@@ -62,10 +132,99 @@ export default {
     position: absolute;
     bottom: px2rem(48);
     left: 0;
-    background: yellow;
+    background: white;
+    box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, 0.15);
     width: 100%;
     height: px2rem(60);
+    z-index: 101;
     .setting-font-size {
+      display: flex;
+      height: 100%;
+      .preview {
+        flex: 0 0 px2rem(40);
+        @include center;
+      }
+      .select {
+        display: flex;
+        flex: 1;
+        .select-wrapper {
+          flex: 1;
+          display: flex;
+          align-items: center;
+          &:first-child {
+            .line {
+              &:first-child {
+                border-top: none;
+              }
+            }
+          }
+          &:last-child {
+            .line {
+              &:last-child {
+                border-top: none;
+              }
+            }
+          }
+          .line {
+            flex: 1;
+            height: 0;
+            border-top: px2rem(1) solid #ccc;
+          }
+          .point-wrapper {
+            position: relative;
+            flex: 0 0 0;
+            width: 0;
+            height: px2rem(7);
+            border-left: px2rem(1) solid #ccc;
+            .point {
+              position: absolute;
+              top: px2rem(-8);
+              left: px2rem(-10);
+              width: px2rem(20);
+              height: px2rem(20);
+              border-radius: 50%;
+              background: white;
+              border: px2rem(1) solid #ccc;
+              box-shadow: 0 px2rem(4) px2rem(2) rgba(0, 0, 0, 0.15);
+              @include center;
+              .small-point {
+                width: px2rem(5);
+                height: px2rem(5);
+                background: black;
+                border-radius: 50%;
+              }
+            }
+          }
+        }
+      }
+    }
+    .setting-theme {
+      height: 100%;
+      display: flex;
+      .setting-theme-item {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        padding: px2rem(5);
+        box-sizing: border-box;
+        .preview {
+          flex: 1;
+          border: px2rem(1) solid #ccc;
+          box-sizing: border-box;
+          &.no-border {
+            border: none;
+          }
+        }
+        .text {
+          flex: 0 0 px2rem(30);
+          font-size: px2rem(14);
+          color: #999;
+          @include center;
+          &.selected {
+            color: #333;
+          }
+        }
+      }
     }
   }
 }
